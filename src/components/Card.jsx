4ec0ce360b3ee80b3a9b05a3e8cc42d7f6488aab
Martín PC <bergesio90@gmail.com>
2024-07-29 16:11:46 +0200

    styles card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,23 @@
+import AddToCart from '../assets/images/icon-add-to-cart.svg';
+
 export const Card = ({ image, name, category, price }) => {
 	return (
 		<article className='flex flex-col'>
 			<img
 				src={image}
 				alt='Image dessert'
+				className='rounded-md'
 			/>
-			<button className='border-solid border border-rose-900 rounded-2xl p-2 px-6 self-center -mt-5 bg-white'>
+			<button className='border-solid border border-[--Rose-900] rounded-3xl p-2 px-6 self-center -mt-5 bg-white flex gap-2 items-center font-bold'>
+				<img
+					src={AddToCart}
+					alt='add to cart'
+				/>
 				Add to card
 			</button>
-			<p>{category}</p>
-			<p>{name}</p>
-			<p>{price}</p>
+			<p className='text-[--Rose-400]'>{category}</p>
+			<p className='text-[--Rose-900] font-bold'>{name}</p>
+			<p className='text-[--Red] font-bold'>${price}</p>
 		</article>
 	);
 };
